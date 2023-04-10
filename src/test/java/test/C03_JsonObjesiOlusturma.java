2059511528e9fe71db11275322920a07335c0faf
gulhayatguder <gulhayatguder31@gmail.com>
2023-04-10 09:36:12 +0200

    first commit

---
+++ src/test/java/test/C03_JsonObjesiOlusturma.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class C03_JsonObjesiOlusturma {
+    /*
+    Asagidaki JSON Objesini olusturup konsolda yazdirin.
+    {
+        "title":"Ahmet",
+        "body":"Merhaba",
+        "userId":1
+    }
+    */
+    @Test
+    public void jsonObje01(){
+        JSONObject ilkJsonObje=new JSONObject();
+    }
+}
